644d6ebc814b924c4d8d6e10f61dd45b9238b22b
newgenius <jonathantchuente@gmail.com>
2021-04-22 13:49:00 +0100

    first deploy of jrme apiv2

--- config/database.js
+++ config/database.js
@@ -4,12 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', process.env.hostDB || '127.0.0.1'),
+        uri: env('DATABASE_URI', process.env.DATABASE_URI),
         srv: env.bool('DATABASE_SRV', process.env.srv ? true : false),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', process.env.nameDB||'jrmeDatabase'),
-        username: env('DATABASE_USERNAME', process.env.userDB || 'geek'),
-        password: env('DATABASE_PASSWORD', process.env.passDB || 'Djansan@2003'),
+        database: env('DATABASE_NAME', process.env.nameDB||'jrmeDatabase')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
