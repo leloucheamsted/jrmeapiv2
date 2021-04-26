43734755d29a0baf1c0d96d24a9d159f644bf638
newgenius <jonathantchuente@gmail.com>
2021-04-26 18:43:12 +0100

    resolve

--- api/student/controllers/student.js
+++ api/student/controllers/student.js
@@ -18,33 +18,33 @@ module.exports = {
         }, process.env.JWT_SECRET);
 
 
-       return ctx.send({token: token, user: entity}, 200);
+        return ctx.send({ token: token, user: entity }, 200);
 
 
     },
 
-    async verifnumber(ctx){
+    async verifnumber(ctx) {
 
         const data = ctx.request.body;
 
         const number = data.number;
 
-        if(!number){
-            return ctx.send({message: "bas data"}, 400);
+        if (!number) {
+            return ctx.send({ message: "bas data" }, 400);
         }
 
-        let student = await strapi.services.student.findOne({number});
+        let student = await strapi.services.student.findOne({ number });
 
-        if(student){
-            return ctx.send({code: 402}, 200);
+        if (student) {
+            return ctx.send({ code: 402 }, 200);
         }
 
-        return ctx.send({code: 200}, 200);
+        return ctx.send({ code: 200 }, 200);
 
 
     },
 
-    async login(ctx){
+    async login(ctx) {
 
         const data = ctx.request.body;
 
@@ -52,33 +52,42 @@ module.exports = {
         let password = data.password;
 
 
-        if(!password){
-            return ctx.send({message: "Invalid password"}, 403);
+        if (!password) {
+            return ctx.send({ message: "Invalid password" }, 403);
         }
 
-        
+
         let student = await strapi.services.student.findOne({ number });
 
-        if(student == null){
-            return ctx.send({message: "Invalid credentials"}, 403);
+        if (student == null) {
+            return ctx.send({ message: "Invalid credentials" }, 403);
         }
 
-        let isValidPassword =  await bcrypt.compare(password, student.password);
+        let isValidPassword = await bcrypt.compare(password, student.password);
 
-        if(isValidPassword){
-            return ctx.send({message: "Invalid credentials"}, 403);
+        if (isValidPassword) {
+            return ctx.send({ message: "Invalid credentials" }, 403);
         }
 
-        const token = jwt.sign({id: student._id }, process.env.JWT_SECRET);
+        const token = jwt.sign( {id: student._id }, process.env.JWT_SECRET);
 
-        return ctx.send({token: token, user: student}, 200);
+        return ctx.send({ token: token, user: student }, 200);
 
 
     },
 
-    async me(ctx){
+    async subjects(ctx) {
 
-        
+        let subjects = await strapi.services.serie.findOne({_id: ctx.state.user.serie._id}, ["subjects"]);
+
+        return ctx.send({ m: subjects }, 200);
+
+
+    },
+
+    async me(ctx) {
+
+
 
     }
 
