9d2db1d321d13cc12efb375578c4b679eaf70f4d
newgenius <jonathantchuente@gmail.com>
2021-04-27 10:19:09 +0100

    red

--- config/database.js
+++ config/database.js
@@ -5,13 +5,13 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         uri: env('DATABASE_URI', process.env.DATABASE_URI||"mongodb://127.0.0.1:27017"),
-        srv: env.bool('DATABASE_SRV', process.env.srv ? true : false),
+        srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', process.env.nameDB||'jrmeDatabase')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', process.env.DATABASE_URI ? true:false),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
